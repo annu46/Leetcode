18f321ffb218730193243203775337097541abda
annu46 <109428524+annu46@users.noreply.github.com>
2024-03-11 16:14:58 +0530

    Time: 1206 ms (12.46%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+SELECT   visited_on, 
+         SUM(SUM(amount)) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)   AS'amount',
+         ROUND(CAST(SUM(SUM(amount)) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS Float)/7.0 ,2) AS'average_amount' 
+FROM     Customer 
+GROUP BY visited_on
+ORDER BY visited_on
+OFFSET 6 ROWS  
+
